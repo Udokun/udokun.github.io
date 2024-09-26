1aa91f29916ab76d4ed2446e39e7ddee1b005e57
Udokun <udokun@yandex.ru>
2024-09-26 13:33:55 +0300

    update4

--- src/js/script.js
+++ src/js/script.js
@@ -7,12 +7,18 @@ $(document).ready(function(){
     autoplaySpeed: 1200,
     arrows: false,
   });
-});
-
-$(document).ready(function(){
+  
   $('.carousel__inner').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 3,
+    // prevArrow: <button type="button" class="slick-prev">Previous</button>,
+    // nextArrow: <button type="button" class="slick-next">Next</button>
   });
+
+  $('ul.calendar__tabs').on('click', 'li:not(.calendar__tab_active)', function(){
+    $(this)
+    .addClass('calendar__tab_active').siblings().removeClass('calendar__tab_active')
+    .closest('div.container').find('div.calendar__content').removeClass('calendar__content_active').eq($(this).index()).addClass('calendar__content_active');
+  })
 });
